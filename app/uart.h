9d31379cbb3a4e1d1c9ca1dcdbf9cba1fb6d9518
Narukara <narukara17@gmail.com>
2021-09-11 21:51:35 +0800

    driver for LCD

--- app/uart.h
+++ app/uart.h
@@ -3,6 +3,7 @@
 
 #include "stm32f10x.h"
 
+void uart_init();
 void uart_send_bit(u8 data);
 void uart_send(const char* s);
 
--- main.c
+++ main.c
@@ -1,46 +1,35 @@
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
+#include "stm32f10x_spi.h"
 #include "stm32f10x_usart.h"
 
+#include "ILI9341.h"
+#include "spi.h"
 #include "systick.h"
 #include "uart.h"
-
-/**
- * PC13 is a LED
- */
 
 int main() {
     SCB->CCR |=
         SCB_CCR_STKALIGN_Msk;  // set STKALIGN, for Cortex-M3 r1p0 and r1p1
-    RCC_APB2PeriphClockCmd(
-        RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_USART1,
-        ENABLE);
-    GPIO_Init(GPIOC, &(GPIO_InitTypeDef){.GPIO_Pin = GPIO_Pin_13,
-                                         .GPIO_Speed = GPIO_Speed_2MHz,
-                                         .GPIO_Mode = GPIO_Mode_Out_PP});
-    // config USART1
-    GPIO_Init(GPIOA, &(GPIO_InitTypeDef){.GPIO_Pin = GPIO_Pin_9,  // TX
-                                         .GPIO_Speed = GPIO_Speed_2MHz,
-                                         .GPIO_Mode = GPIO_Mode_AF_PP});
-    GPIO_Init(GPIOA, &(GPIO_InitTypeDef){.GPIO_Pin = GPIO_Pin_10,  // RX
-                                         .GPIO_Speed = GPIO_Speed_2MHz,
-                                         .GPIO_Mode = GPIO_Mode_IN_FLOATING});
-    USART_Init(USART1, &(USART_InitTypeDef){
-                           .USART_BaudRate = 115200,
-                           .USART_WordLength = USART_WordLength_9b,
-                           .USART_StopBits = USART_StopBits_1,
-                           .USART_Parity = USART_Parity_Odd,
-                           .USART_Mode = USART_Mode_Rx | USART_Mode_Tx});
-    USART_Cmd(USART1, ENABLE);
 
+    uart_init();
     uart_send("Hi");
-
-    while (1) {
-        systick_delay_us(1000000);
-        GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_SET);
-        systick_delay_us(1000000);
-        GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_RESET);
+    spi_init();
+    ILI9341_init();
+    addr_set();
+    for (u16 i = 0; i < 320; i++) {
+        for (u8 j = 0; j < 240; j++) {
+            send_color(i / 10 % 32, 0, j / 8 % 32);
+        }
     }
+    while (1) {
+        __DSB();
+        __WFI();
+    }
+}
+
+void SysTick_Handler(void) {
+    ;
 }
 
 #ifdef USE_FULL_ASSERT
